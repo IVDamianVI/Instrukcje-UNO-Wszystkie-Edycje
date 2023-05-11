5d6d49b97cc1075fa48b4cc9711c0b5dc8db496c
Damian <1damiangrubecki1@gmail.com>
2023-05-11 15:01:58 +0200

    Zwiększenie czasu animacji ładowania

--- js/script-loader.js
+++ js/script-loader.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Losowy czas w milisekundach od 1000 do 2000
-  var randomTime = Math.floor(Math.random() * 1000) + 1000;
+  var randomTime = Math.floor(Math.random() * 2000) + 1000;
 
   // Po losowym czasie dodajemy klasę "loaded" do body
   setTimeout(function () {
